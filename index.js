8957409be4b6279ba6ea8d67442d9da2c1030783
liyang <1711467488@qq.com>
2018-05-16 10:33:12 +0800

    no message

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
-
-const Schema = mongoose.Schema;
-
 const connect = () => {
+  // 如果不是生产环境 就打印日志
   if (process.env.NODE_ENV !== 'production') {
     mongoose.set('debug', true)
   }
+  // mongoose.connect('mongodb://localhost/study');
+  // const db = mongoose.connection;
+  // db.on('error', console.error.bind(console, 'connection error:'));
+  // db.once('open', function() {
+  //   console.log('Hello')
+  // });
   mongoose.connect('mongodb://localhost/study')
     .then(() => {
-      const mySchema = new Schema({
-        title: String
-      }, {
-        timestamps: true
+      // 定义模版
+      const catSchema = new mongoose.Schema({
+        name: String
+      }, { timestamps: true });
+      // 定义模版上的方法
+      catSchema.methods.speak = function () {
+        console.log(`my name is ${this.name}`)
+      };
+      // 由模版编译出 model
+      const CatsModel = mongoose.model('CatsModel', catSchema);
+      // 由 model 创建实体
+      const smallCat = new CatsModel({
+        name: 'cat2'
       });
-      const MyModel = mongoose.model('MyModel', mySchema);
-      const doc = new MyModel({
-        title: 'Hello2'
-      });
-      console.log(doc);
-      doc.save()
+      // 测试
+      smallCat.speak();
+      // 保存
+      smallCat.save()
         .then(() => {
-          console.log('doc saved')
+          console.log('smallCat saved')
         })
     })
     .catch(err => {
